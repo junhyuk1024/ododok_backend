e5d00d3a6a1a37eb0901cb9715815e04bf2d2609
junhyuk1024 <junhyuk919@naver.com>
2026-08-03 20:00:46 +0900

    fix: FeignClient 활성화를 위한 EnableFeignClients 설정 추가

--- src/main/java/com/ododok/server/Main.java
+++ src/main/java/com/ododok/server/Main.java
@@ -2,10 +2,12 @@ package com.ododok.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories // 🔻 JPA Repository 자동 생성 명시
+@EnableJpaRepositories
+@EnableFeignClients // 🔻 OpenFeign 클라이언트 자동 스캔 추가
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
